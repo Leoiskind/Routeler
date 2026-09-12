039df1f80c4394e53a7021b25a7e45b6aaa79fae
f42971lm <leo.morgan@student.manchester.ac.uk>
2026-09-12 17:20:24 +0100

    reorganised the file system

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php
     session_start();
-    //Unsecure, only used because we don't have an absolute host address yet.
-    define ("DEVELOPER_URL", "https://". $_SERVER['HTTP_HOST'] .$_SERVER['REQUEST_URI']."front_page.php");
-    // preg_replace("\/[a-z]+\.php","/front_page.php","https://". $_SERVER['HTTP_HOST'] .$_SERVER['REQUEST_URI'])
+    // Unsecure fallback only used until a production host is configured.
+    $baseUrl = "https://" . $_SERVER['HTTP_HOST'] . dirname($_SERVER['REQUEST_URI']);
+    define("DEVELOPER_URL", rtrim($baseUrl, "/") . "/public/front_page.php");
+
     // Define the location of the service on the Computer Science server.
     define("AUTHENTICATION_SERVICE_URL", "http://studentnet.cs.manchester.ac.uk/authenticate/");
 
-    // Define the location of CAS's logtout service on the Computer Science server.
+    // Define the location of CAS's logout service on the Computer Science server.
     define("AUTHENTICATION_LOGOUT_URL", "http://studentnet.cs.manchester.ac.uk/systemlogout.php");
 
     // Locate the Authenticator class.
-    require_once("Authenticator.php");
+    require_once __DIR__ . "/src/Authenticator.php";
 
     Authenticator::validateUser();
     exit;
