65315149e0abbe6126b41de474a788692f9effaa
f42971lm <leo.morgan@student.manchester.ac.uk>
2026-09-13 14:47:43 +0100

    implemented docker, .env etc.

--- config/db.php
+++ config/db.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 $host = getenv('DB_HOST') ?: '127.0.0.1';
 $port = getenv('DB_PORT') ?: '3306';
 $name = getenv('DB_NAME') ?: 'routeler';
+$user = getenv('DB_USER') ?: 'routeler';
 $pass = getenv('DB_PASS');
 
 if ($pass===false){
@@ -14,8 +15,7 @@ if ($pass===false){
 $dsn = "mysql:host=$host;port=$port;dbname=$name;charset=utf8mb4";
 
 $conn = new PDO($dsn, $user, $pass, [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO:: ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES => false,
+    PDO::ATTR_ERRMODE               => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE   => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES      => false,
 ]);
-?>
